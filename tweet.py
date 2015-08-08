c6f51179da9707942bfc8dc9d6ab6c1aaaec0c80
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-08-08 14:33:35 -0700

    keys

--- tweet.py
+++ tweet.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 import tweepy
 from tweepy.api import API
+import os
 
-API_KEY = 'OxU59qSyfMzlseWHFBFiv2Hgq'
-API_SECRET = 'hide'
-ACCESS_TOKEN = '51948357-DAgrNVcLoZGW9SONcLYEoOwGd3lsoCdPSppEV3azw'
-ACCESS_TOKEN_SECRET = 'rfWikqA5ZronG8hNHzFKsaZiiQzwyvs9YqCjoxfRJHK1L'
+API_KEY = os.environ['API_KEY']
+API_SECRET = os.environ['API_SECRET']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 key = tweepy.OAuthHandler(API_KEY, API_SECRET)
 key.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
 
 class Stream2Screen(tweepy.StreamListener):
     def __init__(self, api=None):
@@ -18,7 +20,8 @@ class Stream2Screen(tweepy.StreamListener):
     def on_status(self, status):
         print status.text.encode('utf8')
         self.n = self.n+1
-        if self.n < self.m: return True
+        if self.n < self.m:
+            return True
         else:
             print 'tweets = '+str(self.n)
             return False
@@ -34,7 +37,8 @@ class Stream2File(tweepy.StreamListener):
     def on_status(self, status):
         self.output.write(status.text.encode('utf8') + "\n")
         self.n = self.n+1
-        if self.n < self.m: return True
+        if self.n < self.m:
+            return True
         else:
             self.output.close()
             print 'tweets = '+str(self.n)
@@ -42,4 +46,3 @@ class Stream2File(tweepy.StreamListener):
 
 stream = tweepy.streaming.Stream(key, Stream2Screen())
 stream.filter(track=['en'], languages=['en'])
-
