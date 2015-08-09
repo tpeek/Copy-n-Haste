34fccf72cefc686a0e3b0cd3b5576fef9e02ff63
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-08-08 18:02:44 -0700

    basic home page

--- CopyHaste/CopyHaste/settings.py
+++ CopyHaste/CopyHaste/settings.py
@@ -44,9 +44,29 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates').replace('\\', '/')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+            ],
+        },
+    },
+]
+
 ROOT_URLCONF = 'CopyHaste.urls'
 
 WSGI_APPLICATION = 'CopyHaste.wsgi.application'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Database
--- CopyHaste/CopyHaste/urls.py
+++ CopyHaste/CopyHaste/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from .views import home_view
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'CopyHaste.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', home_view, name='homepage'),
 )
