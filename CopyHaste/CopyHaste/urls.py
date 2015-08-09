35f919cc339dd58d22e7d1358539411b625beae7
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-08-09 13:26:44 -0700

    admin

--- CopyHaste/CopyHaste/urls.py
+++ CopyHaste/CopyHaste/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .views import home_view
 
 urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home_view, name='homepage'),
     url(r'^accounts/', include('registration.backends.default.urls')),
