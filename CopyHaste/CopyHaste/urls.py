97e81e53ac068927f41e4045bc05002831d2c408
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-08-09 13:33:30 -0700

    fix url patterns

--- CopyHaste/CopyHaste/urls.py
+++ CopyHaste/CopyHaste/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from .views import home_view
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home_view, name='homepage'),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^profile/', include('cnh_profile.urls')),
-)
+]
