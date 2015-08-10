27ad75c337d6a1e26f822a583ef704fd66645fd6
HeyIamJames <jameshemmaplardh@gmail.com>
2015-08-10 14:15:12 -0700

    adding angular js and some other feautures, server is running

--- tutorial/tohdoh/admin.py
+++ tutorial/tohdoh/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from todo.models import TodoList, TodoItem
+from tohdoh.models import TodoList, TodoItem
 
 admin.site.register(TodoList)
 admin.site.register(TodoItem)
