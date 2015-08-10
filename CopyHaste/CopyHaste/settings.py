fa4e9c6c0ccc2b435e3c94dd937f25f45ce49926
Karen Wong <karen.wsywong@gmail.com>
2015-08-10 11:57:52 -0700

    change static url so css will render

--- CopyHaste/CopyHaste/settings.py
+++ CopyHaste/CopyHaste/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 import dj_database_url
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 SECRET_KEY = os.environ['SECRET_KEY']
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'registration',
     'cnh_profile',
     'bootstrap3',
+    'debug_toolbar',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -71,7 +72,10 @@ ROOT_URLCONF = 'CopyHaste.urls'
 WSGI_APPLICATION = 'CopyHaste.wsgi.application'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Database
@@ -99,7 +103,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # Email
 EMAIL_USE_TLS = True
