43a37217518c626f83944cee88264e5c8deaa43e
johnsonjjew@gmail.com <johnsonjjew@gmail.com>
2015-08-10 14:16:14 -0700

    added prelim table for scores

--- CopyHaste/cnh_scores/views.py
+++ CopyHaste/cnh_scores/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+class UserScoreView(TemplateView):
+    template_name = 'scores.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(UserScoreView, self).get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['scores'] = self.request.user.scores
+        return context
