de2c55e7c34d7b49e6d9054578d1c33f5aa4d317
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-08-10 14:17:31 -0700

    a whole lot of messing around

--- CopyHaste/CopyHaste/views.py
+++ CopyHaste/CopyHaste/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+with open("CopyHaste/test1.txt", "r") as myfile:
+    data1 = myfile.read().replace('\n', '')
+
+with open("CopyHaste/test1.txt", "r") as myfile:
+    data2 = myfile.read()
+
 
 def home_view(request):
-    return render(request, 'typingtest2.html')
+    return render(request, 'typingtest2.html', {'data2': data2})
 
 
 def auth_view(request):
