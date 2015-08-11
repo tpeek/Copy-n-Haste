191fbc6c0d7b40a1c482902ff4e76d0880ba75cb
johnsonjjew@gmail.com <johnsonjjew@gmail.com>
2015-08-10 20:07:12 -0700

    added scores and matches

--- CopyHaste/cnh_scores/views.py
+++ CopyHaste/cnh_scores/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import UserScores
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -8,6 +10,8 @@ class UserScoreView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(UserScoreView, self).get_context_data(**kwargs)
-        context['user'] = self.request.user
-        context['scores'] = self.request.user.scores
+        try:
+            context['users'] = User.objects.all()
+        except UserScores.DoesNotExist:
+            pass
         return context
