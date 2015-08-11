0a9c7549b35bfd528b1dbf0f3bdee9f7d4f7db91
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-08-11 16:10:06 -0700

    multiplayer template

--- CopyHaste/typing_test/urls.py
+++ CopyHaste/typing_test/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import play_view
+from .views import play_view, multi_play_view
 
 urlpatterns = [
-    url(r'^', play_view, name='typing_test'),
+    url(r'^$', play_view, name='typing_test'),
+    url(r'^multi/', multi_play_view, name='multi_typing_test'),
 ]
