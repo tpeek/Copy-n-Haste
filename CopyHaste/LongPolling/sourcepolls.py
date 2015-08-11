ba46ece0bfa65e1d4ec08eeb1606a69dd9ef6e86
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-08-11 12:51:27 -0700

    Update sourcepolls.py
    adding poller and maping

--- CopyHaste/LongPolling/sourcepolls.py
+++ CopyHaste/LongPolling/sourcepolls.py
@@ -24,3 +24,26 @@ TIMEOUT = 1000
 # Commonly used flag setes
 READ_ONLY = select.POLLIN | select.POLLPRI | select.POLLHUP | select.POLLERR
 READ_WRITE = READ_ONLY | select.POLLOUT
+
+# Set up the poller
+poller = select.poll()
+poller.register(server, READ_ONLY)
+
+# Map file descriptors to socket objects
+fd_to_socket = { server.fileno(): server,
+               }
+
+
+  # Handle inputs
+        if flag & (select.POLLIN | select.POLLPRI):
+
+            if s is server:
+                # A "readable" server socket is ready to accept a connection
+                connection, client_address = s.accept()
+                print >>sys.stderr, 'new connection from', client_address
+                connection.setblocking(0)
+                fd_to_socket[ connection.fileno() ] = connection
+                poller.register(connection, READ_ONLY)
+
+                # Give the connection a queue for data we want to send
+                message_queues[connection] = Queue.Queue()
