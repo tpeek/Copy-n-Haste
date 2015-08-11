d0b69ad91ed6b3a984a22b347d0c4b6d65480cfe
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-08-11 12:28:47 -0700

    adding socket/polls que

---
+++ CopyHaste/LongPolling/sourcepolls.py
@@ -0,0 +1,19 @@
+import select
+import socket
+import sys
+import Queue
+
+# Create a TCP/IP socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setblocking(0)
+
+# Bind the socket to the port
+server_address = ('localhost', 10000)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+server.bind(server_address)
+
+# Listen for incoming connections
+server.listen(5)
+
+# Keep up with the queues of outgoing messages
+message_queues = {}
