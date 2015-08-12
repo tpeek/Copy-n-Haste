c320c98cac8caccc0d57fd0eb595b52ca53c5c57
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-08-12 13:41:05 -0700

    user text is sent to server ajax

--- CopyHaste/typing_test/views.py
+++ CopyHaste/typing_test/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 with open("typing_test/test1.txt", "r") as myfile:
     data2 = myfile.read().replace('\n', '\\n')
@@ -6,8 +7,13 @@ with open("typing_test/test1.txt", "r") as myfile:
 
 
 def play_view(request):
-    return render(request, 'typingtest2.html', {'data2': data2})
+        return render(request, 'typingtest2.html', {'data2': data2})
 
 
+@csrf_exempt
 def multi_play_view(request):
-    return render(request, 'typingtest3.html', {'data2': data2})
+    if request.method == 'POST':
+        print request.POST['user_input']
+        return render(request, 'typingtest3.html', {'data2': data2})
+    else:
+        return render(request, 'typingtest3.html', {'data2': data2})
