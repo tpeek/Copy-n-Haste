8a751057de6d5d1e9bdbb910bab13d22b12dd557
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-08-12 18:29:25 -0700

    views

--- CopyHaste/typing_test/views.py
+++ CopyHaste/typing_test/views.py
@@ -28,23 +28,18 @@ def multi_play_view(request):
 
 @csrf_exempt
 def matchmaking_view(request):
-    if request.method == 'POST':
-        r = redis.StrictRedis(host='localhost', port=6379, db=0)
-        opponent = ''
-        if r.get('host') is None:
-            r.set('host', request.user.username)
-            while r.get('guest') is None:
-                pass
-            opponent = r.get('guest')
-        else:
-            r.set('guest', request.user.username)
-            opponent = r.get('host')
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    opponent = ''
+    if r.get('host') is None:
+        r.set('host', request.user.username)
+        while r.get('guest') is None:
+            pass
+        opponent = r.get('guest')
     else:
-        r = redis.StrictRedis(host='localhost', port=6379, db=0)
-        r.set(request.user.username, request.POST['user_input'])
-        return render(request, 'typingtest3.html', {'opponent': opponent,
-                                                    'data2': data2})
-    return render(request, 'typingtest3.html', {'data2': data2})
+        r.set('guest', request.user.username)
+        opponent = r.get('host')
+    return render(request, 'typingtest3.html', {'opponent': opponent,
+                                                'data2': data2})
 
 
 @csrf_exempt
