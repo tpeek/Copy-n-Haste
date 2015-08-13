292b31a36ccc5b91957bca405f7a9d9e82ec4f50
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-08-12 18:06:03 -0700

    starting test class

--- CopyHaste/cnh_scores/tests.py
+++ CopyHaste/cnh_scores/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# Create your tests here.
+from __future__ import unicode_literals
+from django.test import TestCase
+from django.db import DataError
+from django.contrib.auth.models import User
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import TransactionTestCase
+from django.test.utils import override_settings
+import factory
+from faker import Faker
+from splinter import Browser
+from .models import CNHProfile
+
+
+fake = Faker()
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = fake.username()
+    email = fake.email()
+
+
+# # # # # # # # # # # # #
+# Unit Tests for Models #
+# # # # # # # # # # # # #
