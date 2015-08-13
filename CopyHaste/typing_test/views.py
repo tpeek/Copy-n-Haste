eb26edb23a22a2f03c43a71f2c8365800ab99170
johnsonjjew@gmail.com <johnsonjjew@gmail.com>
2015-08-12 17:23:10 -0700

    views updated

--- CopyHaste/typing_test/views.py
+++ CopyHaste/typing_test/views.py
@@ -16,6 +16,26 @@ def multi_play_view(request):
     if request.method == 'POST':
         r = redis.StrictRedis(host='localhost', port=6379, db=0)
         r.set(request.user.username, request.POST['user_input'])
-        return render(request, 'typingtest3.html', {'data2': data2})
+        opponent_data = r.get(request.POST['opponent'])
+        return render(request, 'typingtest3.html',
+                      {'opponent_data': opponent_data, 'data2': data2})
     else:
         return render(request, 'typingtest3.html', {'data2': data2})
+
+
+@csrf_exempt
+def matchmaking_view(request):
+    if request.method == 'POST':
+        r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        opponent = ''
+        if r.get('host') is None:
+            r.set('host', request.user.username)
+            while r.get('guest') is None:
+                pass
+            opponent = r.get('guest')
+        else:
+            r.set('guest', request.user.username)
+            opponent = r.get('host')
+    else:
+        return render(request, 'typingtest3.html', {'opponent': opponent,
+                                                    'data2': data2})
