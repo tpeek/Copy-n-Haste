281566df84ea065ef31c5519b2d6bd10b630bb14
johnsonjjew@gmail.com <johnsonjjew@gmail.com>
2015-08-12 17:47:27 -0700

    changed matchmaking view

--- CopyHaste/typing_test/urls.py
+++ CopyHaste/typing_test/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import play_view, multi_play_view, get_content_view
+from .views import play_view, matchmaking_view, multi_play_view, get_content_view
 
 urlpatterns = [
     url(r'^$', play_view, name='typing_test'),
-    url(r'^multi/', multi_play_view, name='multi_typing_test'),
+    url(r'^multi/play', multi_play_view, name='multi_typing_test'),
+    url(r'^multi/', matchmaking_view, name='matchmaking'),
     url(r'^content/', get_content_view)
 ]
