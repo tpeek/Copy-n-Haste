69c610d0b7b85f99f53b933d66c09cd5e6c66c29
HeyIamJames <jameshemmaplardh@gmail.com>
2015-08-15 20:15:44 -0700

    making a giant pull

--- CopyHaste/CopyHaste/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def home_view(request):
-    return render(request, 'home.html')
-
-
-def auth_view(request):
-    return
--- CopyHaste/cnh_profile/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CNHProfileConfig(AppConfig):
-    name = 'cnh_profile'
-    verbose_name = 'Copy N Haste Profile'
-
-    def ready(self):
-        import handlers
