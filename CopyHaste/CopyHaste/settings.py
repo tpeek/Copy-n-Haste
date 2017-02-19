c2892ca214dd040c3a42c8fbe284734cd008cd9f
Tyler Peek <tyler.peek@eagles.ewu.edu>
2017-02-19 13:51:31 -0800

    make DEBUG checking more robust

--- CopyHaste/CopyHaste/settings.py
+++ CopyHaste/CopyHaste/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-DEBUG = os.environ['DEBUG']
+DEBUG = True if os.environ.get('DEBUG') == 'True' else False
 
 TEMPLATE_DEBUG = False
 
